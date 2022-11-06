94b5b14e42f859b5c5a613f90c01b0b1242fe732
李芳军 <lifangjun@lifangjundeMacBook-Pro.local>
2022-11-06 20:07:25 +0800

    feature: add new api

--- apis/example/user.py
+++ apis/example/user.py
@@ -1,5 +1,7 @@
-from flask_restful import Resource
-from common.return_data import get_return_data, Success
+import logging
+
+from flask_restful import Resource, request
+from common.return_data import get_return_data, Success, Unsuccessfully
 from apis.models.user import User
 from apis.schemas.user import UserSchema
 import json
@@ -8,6 +10,24 @@ import json
 class UserViewSet(Resource):
 
     def get(self):
-        user = User.query.all()
+        user = User.query.filter(User.is_delete==False).all()
         user_info = UserSchema().dumps(user, many=True)
         return get_return_data(Success, json.loads(user_info))
+
+    def post(self):
+        data = json.loads(request.data)
+        user = User.create(**data)
+        data = UserSchema().dumps(user)
+        return get_return_data(Success, json.loads(data))
+
+    def delete(self):
+        try:
+            id = json.loads(request.args.get("id"))
+            user = User.query.get(id)
+            user.is_delete = True
+            user.save()
+            return get_return_data(Success, {})
+        except Exception as e:
+            print(str(e))
+            return get_return_data(Unsuccessfully, {}, msg=str(e))
+
